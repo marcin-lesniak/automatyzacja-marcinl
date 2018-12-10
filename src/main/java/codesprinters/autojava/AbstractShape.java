55a194a4235505aceca4320f5298cdf7c9a0f5dd
mlesniak <marcin.lesniak18@gmail.com>
2018-12-10 21:18:36 +0100

    Zadanie2 - Shapes

---
+++ src/main/java/codesprinters/autojava/AbstractShape.java
@@ -0,0 +1,11 @@
+package codesprinters.autojava;
+
+public abstract class AbstractShape implements Shape {
+	
+	@Override
+	public String getName() {
+
+		return this.getClass().getSimpleName();
+	}
+
+}
